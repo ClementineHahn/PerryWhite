d9689b0cb51c85d7ba4fef5c492fcf1da384dbb5
clémentine <clementine.hahn@gmail.com>
2018-02-20 16:29:20 -0500

    Affichage de 2 infos en même temps. Fixes #5

--- Creation-Bouton-Perry.js
+++ Creation-Bouton-Perry.js
@@ -1,11 +1,10 @@
 function onOpen() {
     DocumentApp.getUi().createMenu('Bonjour Perry').addItem('Créer une PR', 'createPR')
 .addToUi();
-
 }
 
-function createPR () {
- // Logger.log('coucou') 
- // Logger.log(1+2)
-  DocumentApp.getUi().alert('Tu as créé un bouton qui fait une action simple ! Bravo !')
+function createPR(){
+  var body = DocumentApp.getActiveDocument().getBody();
+  var firstChild = body.getChild(0)
+  DocumentApp.getUi().alert("*Type du 1stchild* sa mère" + ": " + firstChild.getType() + " // *Contenu du 1st child* la pute: " + firstChild.asText().getText())
 }
