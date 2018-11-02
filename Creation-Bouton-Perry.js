31fc2be9f5ea5a546aa68aaa9db99c0f76e6448c
David Bruant <bruant.d@gmail.com>
2018-11-02 15:41:49 +0100

    Accumulation de tout le texte du document dans une seule variable
    Fixes #11

--- Creation-Bouton-Perry.js
+++ Creation-Bouton-Perry.js
@@ -6,11 +6,18 @@ function onOpen() {
 function createPR(){
   var body = DocumentApp.getActiveDocument().getBody();
   var Number = body.getNumChildren()
+  
+  var allText = '';
 
   for(var Numero = 0 ; Numero < Number ; Numero+=1){
     var child = body.getChild(Numero)
     var ParagraphHeading = child.getHeading()
     
-    DocumentApp.getUi().alert(child.getType() + " / " + ParagraphHeading + " : \"\ " + child.asText().getText() + " \"\ " )
-  }
+    allText = allText + '\n' + child.asText().getText()
+    
+    // DocumentApp.getUi().alert()
+    //console.log(child.getType() + " / " + ParagraphHeading + " : \"\ " + child.asText().getText() + " \"\ " )
+  } 
+  
+  console.log(allText)
 }
